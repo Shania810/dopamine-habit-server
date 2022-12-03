14a083cb70ce0a431ed1192fac74dd21bbe8a9b9
Shania <shaniaantezana@gmail.com>
2022-12-03 13:30:42 -0400

    change model habit

--- models/habit.model.js
+++ models/habit.model.js
@@ -1,5 +1,6 @@
 import {Schema,model} from 'mongoose'
 const habitSchema = new Schema({
+    title:{type:String,required,required:[true,'title is required'],trim: true,lowercase: true},
     description:{type:String,required:[true,'description is required'],trim:true,lowercase:true},
     user:{type:Schema.Types.ObjectId,ref:'User',required:true}
 },{timestamps:true})
