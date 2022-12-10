b27531db6dc76854f55904c48f89f0403c36ba26
Anneliese Kappey <anneliesekappey@gmail.com>
2022-12-10 13:37:16 +0100

    try to pull

--- routes/analysis.routes.js
+++ routes/analysis.routes.js
@@ -1,43 +1,46 @@
-import { Router } from "express";
+import { Router } from 'express'
 const router = Router()
 import Analysis from '../models/analysis.model.js'
-router.get('/analysis',async(req,res,next)=>{
- const {id} = req.user
+router.get('/analysis', async (req, res, next) => {
+  const { id } = req.user
   try {
-    const analysis = await Analysis.find({user: id})
+    const analysis = await Analysis.find({ user: id })
     res.status(200).json(analysis)
   } catch (error) {
     next(error)
   }
 })
-router.post('/analysis',async(req,res,next)=>{
-  const {id} = req.user
+router.post('/analysis', async (req, res, next) => {
+  const { id } = req.user
   const analysis = req.body
   try {
-    const newAnalysis = await Analysis.create({...analysis,user:id})
+    const newAnalysis = await Analysis.create({ ...analysis, user: id })
     res.status(201).json(newAnalysis)
   } catch (error) {
     next(error)
   }
 })
-router.put('/analysis',async(req,res,next)=>{
-   const update = req.body
-   const { id } = req.user 
-   try {
-    const updatedAnalysis = await Analysis.updateOne({user: id},{update})
-    res.status(200).json(updatedAnalysis)
-   } catch (error) {
-    next(error)
-   } 
-})
-router.put('/analysis/habits',async(req,res,next)=>{
-  const habits = req.body
-  const {id} = req.user
+router.put('/analysis', async (req, res, next) => {
+  const update = req.body
+  const { id } = req.user
   try {
-    for(let i = 0; i< habits.length; i++){
-      await Analysis.updateOne({user: id},{$push:{ habits: habits[i]._id}})
+    const updatedAnalysis = await Analysis.updateOne({ user: id }, { update })
+    res.status(200).json(updatedAnalysis)
+  } catch (error) {
+    next(error)
+  }
+})
+router.put('/analysis/habits', async (req, res, next) => {
+  const habits = req.body
+  const { id } = req.user
+  try {
+    for (let i = 0; i < habits.length; i++) {
+      await Analysis.updateOne(
+        { user: id },
+        { $push: { habits: habits[i]._id } }
+      )
     }
-    const analysis = await Analysis.find({user: id})
+    const analysis = await Analysis.find({ user: id })
     await analysis[0].populate('habits')
     res.status(200).json(analysis)
   } catch (error) {
