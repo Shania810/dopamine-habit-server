93a734a98dc83171282950dd4f0c271c6439a8ec
Anneliese Kappey <anneliesekappey@gmail.com>
2022-12-10 15:47:21 +0100

    add max to days in habit model

--- models/habit.model.js
+++ models/habit.model.js
@@ -1,9 +1,23 @@
-import {Schema,model} from 'mongoose'
-const habitSchema = new Schema({
-    title:{type:String,required:[true,'title is required'],trim: true,lowercase: true},
-    description:{type:String,required:[true,'description is required'],trim:true,lowercase:true},
-    frequency:{type:String,lowercase:true},
-    days_completed:{type:Number,default:0},
-    user:{type:Schema.Types.ObjectId,ref:'User',required:true}
-},{timestamps:true})
-export default model('Habit',habitSchema)
+import { Schema, model } from 'mongoose'
+
+const habitSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'title is required'],
+      trim: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'description is required'],
+      trim: true,
+      lowercase: true,
+    },
+    frequency: { type: String, lowercase: true },
+    days_completed: { type: Number, default: 0, max: 7 },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+)
+export default model('Habit', habitSchema)
