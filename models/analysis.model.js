85764e9f61a70fda46bd52906243a5d5a79ed039
Anneliese Kappey <anneliesekappey@gmail.com>
2022-12-10 15:13:14 +0100

    new key analysis model - current_day

--- models/analysis.model.js
+++ models/analysis.model.js
@@ -1,8 +1,13 @@
-import {Schema,model} from 'mongoose'
-const analysisSchema = new Schema({
-    habits:[{type:Schema.Types.ObjectId,ref:'Habit'}],
-    duration_of_analysis:{type:Number,default:7},
-    created_at:{type:Date},
-    user:{type:Schema.Types.ObjectId,ref:'User',required: true}
-},{timestamps:true})
-export default model('Analysis',analysisSchema)
+import { Schema, model } from 'mongoose'
+
+const analysisSchema = new Schema(
+  {
+    habits: [{ type: Schema.Types.ObjectId, ref: 'Habit' }],
+    duration_of_analysis: { type: Number, default: 7 },
+    current_day: { type: Number, default: 1, max: 7 },
+    created_at: { type: Date },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+)
+export default model('Analysis', analysisSchema)
