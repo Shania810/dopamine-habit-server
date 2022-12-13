ba957ce75442c64ccb49ebd14a3957d3a91f4177
Shania <shaniaantezana@gmail.com>
2022-12-13 08:59:06 -0400

    change analysis.model.js

--- models/analysis.model.js
+++ models/analysis.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose'
 const analysisSchema = new Schema(
   {
     habits: [{ type: Schema.Types.ObjectId, ref: 'Habit' }],
-    duration: { type: Number, default: 1,max: [7,'must be least 7'] },
+    duration: { type: Date},
     created_at: { type: Date },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
